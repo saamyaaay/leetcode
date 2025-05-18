0d3c25a9acdaa9a8ed9923a9c9d58f0ee908077a
saamyaaay <128019450+saamyaaay@users.noreply.github.com>
2025-05-18 16:55:36 +0530

    Time: 7 ms (68.25%), Space: 11.5 MB (43.84%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,50 +2,39 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (s.length() < t.length()) return "";
+  unordered_map<char,int>se;
+  unordered_map<char,int>tt;
+  int n = s.size(); int m = t.size();
+  for(int i = 0; i<m ; i++){
+    tt[t[i]]++;
+  }
+  int left = 0;
+  int right = 0;
+  int count = 0;
+  int start = -1;
+  int required = tt.size();
+  int minlength =INT_MAX;
+  for(int right = 0; right<n ; right++){
+   char c = s[right];
+   se[c]++;
 
-        unordered_map<char, int> targetFreq;
-        unordered_map<char, int> windowFreq;
-
-        // Count frequency of chars in t
-        for (char ch : t) {
-            targetFreq[ch]++;
-        }
-
-        int required = targetFreq.size(); // Number of unique chars to match
-        int formed = 0;                   // How many unique chars matched required freq so far
-        int left = 0, right = 0;
-        int minLen = INT_MAX;
-        int startIndex = 0;
-
-        while (right < s.length()) {
-            char ch = s[right];
-            windowFreq[ch]++;
-
-            // If char is part of t and windowFreq matches targetFreq, increment formed
-            if (targetFreq.count(ch) && windowFreq[ch] == targetFreq[ch]) {
-                formed++;
-            }
-
-            // Try to contract window when all chars matched
-            while (left <= right && formed == required) {
-                // Update minimum window
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
-                    startIndex = left;
-                }
-
-                // Remove from window from left side
-                char leftChar = s[left];
-                windowFreq[leftChar]--;
-                if (targetFreq.count(leftChar) && windowFreq[leftChar] < targetFreq[leftChar]) {
-                    formed--;
-                }
-                left++;
-            }
-            right++;
-        }
-
-        return (minLen == INT_MAX) ? "" : s.substr(startIndex, minLen);
+   if(tt.count(c)&& se[c]==tt[c] ){
+    count++;
+   }
+   while (count==required){
+    if (right-left+1<minlength){
+        minlength = right-left+1;
+        start=left;
+    }
+    char lefty= s[left];
+    se[lefty]--;
+    if(tt.count(lefty) && se[lefty]<tt[lefty]){
+        count--;
+    }
+left++;
+   }
+  }
+  if(minlength ==INT_MAX) return "";
+  return s.substr(start,minlength);
     }
 };
