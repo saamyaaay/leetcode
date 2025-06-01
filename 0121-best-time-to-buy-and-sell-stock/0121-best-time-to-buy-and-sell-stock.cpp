22c2693877a628e76d9fb0a08ce1edfee3c66e3e
saamyaaay <128019450+saamyaaay@users.noreply.github.com>
2025-06-01 21:32:46 +0530

    Time: 0 ms (100%), Space: 97.4 MB (26.4%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,13 +4,11 @@ public:
         int buy = prices[0];
         int profit = 0;
         for(int i =0; i<prices.size(); i++){
-            if (buy>prices[i]){
-                buy=prices[i];
+
+            if(prices[i]> buy){
+                profit = max(profit , prices[i]-buy);
             }
-            else{
-                int current = prices[i]- buy;
-                profit = max (current,profit);
-            }
+            buy = min(buy , prices[i]);
         }
         return profit;
     }
